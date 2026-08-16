69ce4cbe1e792e781a1475a13a9d1818db9536b0
Flamzyboi1 <favourobidiaso@gmail.com>
2026-08-16 23:44:30 +0300

    Ensure ECOIGM admin login account exists

---
+++ src/main/java/lv/venta/irrigation/config/DefaultAdminInitializer.java
@@ -0,0 +1,27 @@
+package lv.venta.irrigation.config;
+
+import lv.venta.irrigation.model.AppUser;
+import lv.venta.irrigation.repo.AppUserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultAdminInitializer {
+    @Bean
+    CommandLineRunner createDefaultAdmin(AppUserRepository repo){
+        return args -> {
+            AppUser user=repo.findByUsername("EcoigmAdmin");
+            if(user==null){
+                user=new AppUser();
+                user.setUsername("EcoigmAdmin");
+                user.setPassword("Ecoigm123#");
+                user.setFullName("ECOIGM Administrator");
+                user.setEmail("admin@ecoigm.local");
+                user.setRole("SUPERADMIN");
+            }
+            user.setActive(true);
+            repo.save(user);
+        };
+    }
+}
